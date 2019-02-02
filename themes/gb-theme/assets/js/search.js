658e97f85fd8dd143823f76854f0a6ea190ea277
fernandogelin <fernandogelin@gmail.com>
2019-02-02 09:32:35 -0500

    fix(search): remove / from index.json path in search.js

--- themes/gb-theme/assets/js/search.js
+++ themes/gb-theme/assets/js/search.js
@@ -29,7 +29,7 @@ if(searchQuery){
 
 
 function executeSearch(searchQuery){
-  $.getJSON( "/index.json", function( data ) {
+  $.getJSON( "index.json", function( data ) {
     var pages = data;
     var fuse = new Fuse(pages, fuseOptions);
     var result = fuse.search(searchQuery);
