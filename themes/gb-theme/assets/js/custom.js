6d9b6f43cf8a036c4c946b12141a9186bd050ddc
fernandogelin <fernandogelin@gmail.com>
2019-02-11 08:11:14 -0500

    fix(collapse): fix link collapse

--- themes/gb-theme/assets/js/custom.js
+++ themes/gb-theme/assets/js/custom.js
@@ -21,12 +21,6 @@ $(function () {
         $(`#${targetId}`).removeClass('d-none');
     });
 
-// // Toggle collapse
-//     $('button').click(function () {
-//         const targetId = $(this).attr('data-target');
-//         $(`${targetId}`).removeClass('show');
-//         $(`${targetId}`).addClass('collapse');
-//     })
 
 // Add table class
     $('table').addClass('table');
@@ -34,7 +28,7 @@ $(function () {
 
 // Scroll
     // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+    $('.section-nav-link').on('click', function(event) {
 
       // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== "") {
@@ -48,7 +42,7 @@ $(function () {
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
           scrollTop: $(hash).offset().top
-        }, 800, function(){
+        }, 400, function(){
 
           // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
