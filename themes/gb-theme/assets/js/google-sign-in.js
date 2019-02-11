e1696ae6b67c6f7de0563f01f8969fa8335c21e4
fernandogelin <fernandogelin@gmail.com>
2019-02-11 16:49:22 -0500

    fix(googlesignin):

--- themes/gb-theme/assets/js/google-sign-in.js
+++ themes/gb-theme/assets/js/google-sign-in.js
@@ -5,4 +5,11 @@ function onSignIn(googleUser) {
   $('#loginmessage').text('Logged in as:');
   $('#googleUser').text(profile.getName());
   $('#googleEmail').text(profile.getEmail());
+  $('#googleSubmit').removeAttr('disabled');
 }
+function onSuccess(googleUser) {
+   console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+   $('#googleUser').text(profile.getName());
+   $('#googleEmail').text(profile.getEmail());
+   $('#googleSubmit').removeAttr('disabled');
+ }
