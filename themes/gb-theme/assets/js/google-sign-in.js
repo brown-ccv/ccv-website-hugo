777a95b4c412e91bbbb6ef14c6f01d8090ec93f0
fernandogelin <fernandogelin@gmail.com>
2019-02-11 16:31:33 -0500

    fix(googlesignin):

--- themes/gb-theme/assets/js/google-sign-in.js
+++ themes/gb-theme/assets/js/google-sign-in.js
@@ -1,8 +1,8 @@
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   $('#inputFullName').val(profile.getName());
+  $('#inputEmail').val(profile.getEmail());
+  $('#loginmessage').text('Logged in as:');
+  $('#googleUser').text(profile.getName());
+  $('#googleEmail').text(profile.getEmail());
 }
