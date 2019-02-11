17c2ef190c0db9e929ab12df64f2b53112d001cd
fernandogelin <fernandogelin@gmail.com>
2019-02-11 15:36:48 -0500

    feat(test): test prefill form

--- themes/gb-theme/assets/js/google-sign-in.js
+++ themes/gb-theme/assets/js/google-sign-in.js
@@ -4,4 +4,5 @@ function onSignIn(googleUser) {
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  $('#inputFullName').val(profile.getName());
 }
