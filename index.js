47298d4fb2cded3a38dc7ef59109581e88853e97
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2020-02-19 12:46:00 -0500

    refactor: switch to github graphql api, remove lodash dependency

--- index.js
+++ index.js
@@ -2,39 +2,35 @@ require('dotenv').config()
 
 const fs   = require('fs');
 const request = require('request');
-const Base64 = require('js-base64').Base64;
-const _ = require('lodash');
 const yaml = require('js-yaml');
 
 const token = process.env.GITHUB_TOKEN;
 const user = process.env.GITHUB_USER;
 const organizationList = process.env.ORGANIZATION.split(',');
 
-/**
- * Returns a promise that resolves to a Github API response.
- */
-const githubRequest = (path, returnHeaders) => {
+
+const githubGqlRequest = (query) => {
   return new Promise((resolve, reject) => {
     request({
-      url: `https://api.github.com/${path}`,
+			method: 'POST',
+      url: `https://api.github.com/graphql`,
       headers: {
         'Authorization': `token ${token}`,
         'User-Agent': user,
         'Accept': 'application/vnd.github.v3+json'
-      }
-    }, (err, response) => {
+      },
+			json: true,
+			body: query
+    }, (err, response, body) => {
       if (err) {
-	return reject(err);
+				return reject(err);
       }
 
-      if (returnHeaders) {
-        resolve(response); // send back headers + body
-      } else {
-        resolve(JSON.parse(response.body)); // just send back body
-      }
+      resolve(response.body)
     });
   });
 };
+
 
 /**
  * The procedure below gets a list of repositories for the organization using
@@ -53,84 +49,68 @@ let retrievedData = {}  // (key: organization, value: [] of repos for that org)
 
 for (let i = 0; i < totalOrgs; i++) {
     let organization = organizationList[i];
+
     let filePath = 'data/info-' + organization + '.json';
 
     retrievedData[organization] = [];
-    getPage(`orgs/${organization}/repos?per_page=30`, organization, filePath);
+    getPage(organization, filePath, "");
 }
 
-function getPage(url, organization, filePath) {
+function cursorText(cursor) {
+	if (cursor.length > 0 ) {
+		return `, after: "${cursor}"`
+	} else {
+		return ""
+	}
+}
+
+function getPage(organization, filePath, cursor) {
   let last = false;
-  let nextURL = "";
+	let nextCursor = ""
 
-  githubRequest(url, true).then((values) => {
+	let query = {query: `
+		{ organization(login: "${organization}") {
+			repositories(first: 100${cursorText(cursor)}) {
+				pageInfo {
+					endCursor
+					hasNextPage
+				}
+				nodes {
+					name
+					ready: object(expression: "master:ready.yml") {
+						oid
+						... on Blob {
+							text
+						}
+					}
+				}
+			}
+		}
+	}
+	`}
 
-    if (values.headers['link']) {
-      // if API paginated items
+  githubGqlRequest(query)
+	.then((values) => {
+		let repos = values.data.organization.repositories
 
-      let linksDict = {}; // dictionary (key: rel | value: url)
+		if (repos.pageInfo.hasNextPage) {
+			nextCursor = repos.pageInfo.endCursor
+		} else {
+			last = true
+		}
 
-      // parse returned header for link to next page
-      let headerLinks = values.headers['link'].split(", "); // split previous, next, last links.
+		let readyRepos = repos.nodes.filter(repo => repo.ready)
 
-      for (let linkI = 0; linkI < headerLinks.length; linkI++) {
-        let linkRelSplit = headerLinks[linkI].split(">; rel=\"");
-        let relExtracted = linkRelSplit[1].substring(0, linkRelSplit[1].length-1);
-        let urlExtracted = linkRelSplit[0].substring(1);
-        urlExtracted = urlExtracted.substring("https://api.github.com/".length);
-        linksDict[relExtracted] = urlExtracted;
-      }
+		readyRepos.forEach((repo) => retrievedData[organization].push(yaml.safeLoad(repo.ready.text)))
 
-      // traversing to next page handled below for synchronicity within organization, so needed
-      // info passed over here.
-      if ("next" in linksDict) {
-        nextURL = linksDict["next"];
-      } else {
-        last = true;
-      }
-
-    } else {
-      // if all data returned on single page
-      last = true;
-    }
-
-    // gets the contents of the repos
-    let body = JSON.parse(values.body);
-    return Promise.all(body.map((repo) => {
-      return githubRequest(`repos/${organization}/${repo.name}/contents`, false)
-    }));
-  }).then((values) => {
-    return arr = _.compact(_.flatten(values.map((content) => {
-      return _.filter(content, function(n) {
-        return n.length != 0;
-      })
-    })));
-  }).then((values) => {
-    return values.map((file) => {
-      if (Object.values(file).includes('ready.yml')) {
-        console.log(file.url)
-        return file.url.split('/')[5]
-      };
-    })
-  }).then((values) => {
-    const readyRepos = _.filter(values, (n) => n != undefined)
-    // gets contents from ready.yml of the repos that have docs folder
-    return Promise.all(readyRepos.map((item) => {
-      return githubRequest(`repos/${organization}/${item}/contents/ready.yml`, false);
-    }))
-  }).then((values) => {
-    // decode and save readme content
-    let existingDataList = retrievedData[organization];
-    values.forEach((item) => existingDataList.push(yaml.safeLoad(Base64.decode(item.content))));
-    retrievedData[organization] = existingDataList;
-
-    if (last) {
-      // on last page, write to file
+		if (last) {
+	      // on last page, write to file
       fs.writeFileSync(filePath, JSON.stringify(retrievedData[organization], null, 2));
       console.log('Data written to ' + filePath + ", num items: " + retrievedData[organization].length);
     } else {
       // get data from next page
-      getPage(nextURL, organization, filePath);
+      getPage(organization, filePath, nextCursor);
     }
-  }).catch((err) => console.error(err));
+  })
+  .catch((err) => console.error(err));
 }
